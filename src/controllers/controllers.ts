164bff8a442c3938e2e7c92952540192f1b6d4a0
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-07-25 12:15:44 +0530

    fixed some bugs

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -3,22 +3,26 @@ import { userSchema } from "../validators/validators";
 import { User } from "../models/model";
 
 export const Signup = async (req: Request, res: Response) => {
-    const {username, password} = req.body;
-
-    //check if the coming data is correct
-    const validation = userSchema.safeParse(req.body);
-    if (!validation.success) return res.status(411).json({ message: 'invalid inputs received' });
-
-    //check for existing user
-    const existingUser = await User.findOne({username})
-    if(existingUser) res.status(403).json({message : `user already exists`})
-
-    //if everything is fine create a new user
-    await User.create({
-        username,
-        password
-    });
-
-    res.status(201).json({message : `user created successfully`});
+    try {
+        const {username, password} = req.body;
+    
+        //check if the coming data is correct
+        const validation = userSchema.safeParse(req.body);
+        if (!validation.success) return res.status(411).json({ message: 'invalid inputs received' });
+    
+        //check for existing user
+        const existingUser = await User.findOne({username})
+        if(existingUser) res.status(403).json({message : `user already exists`})
+    
+        //if everything is fine create a new user
+        await User.create({
+            username,
+            password
+        });
+    
+        res.status(201).json({message : `user created successfully`});
+    } catch (error) {
+        console.log(`Error while signingUp`,error)
+    }
     
 };
