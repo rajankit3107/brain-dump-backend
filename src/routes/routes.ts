94b8d4f9b6ad4b30987b10231f0f141b57f90c50
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-07-25 12:23:34 +0530

    fixed some bugs

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,9 @@
-import express from "express";
+import express from 'express';
 import { Signup } from "../controllers/controllers.js";
 
 const router = express.Router();
 
 router.post("/signup", Signup);
+
+export default router;
+
