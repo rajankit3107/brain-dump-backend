1c28cf5c91d7c7242def1cfb93f701ac2420ba2c
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-07-25 16:18:31 +0530

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config";
+import connectDB from "./config.js";
+import router from "./routes/routes.js";
+
 
 const app = express();
 dotenv.config();
@@ -8,6 +10,8 @@ dotenv.config();
 app.use(express.json());
 
 connectDB();
+
+app.use('/api/v1', router)
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
